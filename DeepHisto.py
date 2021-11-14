d48c1bff2c7faabbdaaff08e199e0d6ed90d90b1
Soroush Oskouei <57323986+SoroushOskouei@users.noreply.github.com>
2021-11-15 00:05:10 +0200

    Update DeepHisto.py

--- DeepHisto.py
+++ DeepHisto.py
@@ -124,7 +124,7 @@ components.html(
         
         
 <video style="margin-top:10px; margin-left:20px;" width="800" height="400" controls>
-<source src="https://share.streamlit.io/soroushoskouei/deephistology/tut.mp4" type="video/mp4">
+<source src="https://www.youtube.com/watch?v=kXvmqg8hc70" type="video/mp4">
 </video>
 
 
